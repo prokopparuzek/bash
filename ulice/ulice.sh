e07ca3a2656b05547020b66321e10b08cc66e568
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-03-20 16:38:52 +0100

    init

---
+++ ulice/ulice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tmp1=$(mktemp)
+tmp2=$(mktemp)
+trap 'rm -rf $tmp1, $tmp2' EXIT
+
+# dos2unix
+dos2unix $1
+# swap numbers and streets
+sed -r -e "s/([0-9]+)\,(.*)$/\2,\1/" $1 >$tmp1
+# sort by street
+sort $tmp1 >$tmp2
+# extract dupicities
+sed -r -e "s/(.*),([0-9]+)/\2 \1/" $tmp2|uniq -f 1 -D
